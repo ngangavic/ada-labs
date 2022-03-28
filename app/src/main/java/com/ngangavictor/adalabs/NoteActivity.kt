f8c92620e741684ae7f5fe73cfe12e7d76c8db08
ngangavic <ngangavictor10@gmail.com>
2022-03-28 13:38:56 +0300

    add note fragment

--- app/src/main/java/com/ngangavictor/adalabs/NoteActivity.kt
+++ app/src/main/java/com/ngangavictor/adalabs/NoteActivity.kt
@@ -2,42 +2,38 @@ package com.ngangavictor.adalabs
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
 
 class NoteActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     lateinit var toolbar: Toolbar
-    lateinit var fab:FloatingActionButton
-    lateinit var drawerLayout:DrawerLayout
-    lateinit var navView:NavigationView
+    lateinit var fab: FloatingActionButton
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note)
 
-        toolbar=findViewById(R.id.toolbar)
-        fab=findViewById(R.id.fab)
-        drawerLayout=findViewById(R.id.drawer_layout)
-        navView=findViewById(R.id.nav_view)
+        toolbar = findViewById(R.id.toolbar)
+        fab = findViewById(R.id.fab)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
 
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+
 //        val drawerLayout: DrawerLayout = drawerLayout
 //        val navView: NavigationView = navView
         val navController = findNavController(R.id.nav_host_fragment_content_note)
@@ -50,6 +46,9 @@ class NoteActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        fab.setOnClickListener { view ->
+            navController.navigate(R.id.nav_add_note)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
