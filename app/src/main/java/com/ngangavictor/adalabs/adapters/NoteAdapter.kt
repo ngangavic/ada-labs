7ce934bbfc59550f5f369134756a33c61420f7ab
ngangavic <ngangavictor10@gmail.com>
2022-04-01 21:51:11 +0300

    note rv not showing

--- app/src/main/java/com/ngangavictor/adalabs/adapters/NoteAdapter.kt
+++ app/src/main/java/com/ngangavictor/adalabs/adapters/NoteAdapter.kt
@@ -4,11 +4,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.ngangavictor.adalabs.R
 import com.ngangavictor.adalabs.databinding.RowViewNoteBinding
 import com.ngangavictor.adalabs.models.NoteModel
 
-class NoteAdapter(private val list:ArrayList<NoteModel>): RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+class NoteAdapter(
+    private val list: ArrayList<NoteModel>,
+    var noteItemClickListener: NoteItemClickListener
+) : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteAdapter.NoteViewHolder {
 
@@ -29,12 +31,26 @@ class NoteAdapter(private val list:ArrayList<NoteModel>): RecyclerView.Adapter<N
         return list.size
     }
 
-   inner class NoteViewHolder constructor(
-       private val binding: RowViewNoteBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class NoteViewHolder constructor(
+        private val binding: RowViewNoteBinding
+    ) : RecyclerView.ViewHolder(binding.root), View.OnClickListener {
         fun bind(item: NoteModel) {
             binding.apply {
-
+                binding.textViewTitle.text = item.title
+                binding.textViewNote.text = item.body
+//                binding.root.setOnClickListener(this@NoteViewHolder)
             }
         }
+
+
+        override fun onClick(p0: View?) {
+            if (p0 != null) {
+                noteItemClickListener.itemClicked(p0, list[position])
+            }
+        }
+    }
+
+    interface NoteItemClickListener {
+        fun itemClicked(view: View, noteModel: NoteModel)
     }
 }
--- app/src/main/java/com/ngangavictor/adalabs/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ngangavictor/adalabs/ui/home/HomeFragment.kt
@@ -5,16 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.ngangavictor.adalabs.R
+import com.ngangavictor.adalabs.adapters.NoteAdapter
+import com.ngangavictor.adalabs.databinding.FragmentHomeBinding
+import com.ngangavictor.adalabs.models.NoteModel
 
-class HomeFragment : Fragment() {
+
+class HomeFragment : Fragment(), NoteAdapter.NoteItemClickListener {
 
     private lateinit var homeViewModel: HomeViewModel
 
-    lateinit var textHome:TextView
+    lateinit var textHome: TextView
+
+    lateinit var binding: FragmentHomeBinding
+
+    lateinit var noteAdapter: NoteAdapter
+
+    lateinit var noteList: ArrayList<NoteModel>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,6 +37,10 @@ class HomeFragment : Fragment() {
     ): View? {
         homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+        noteList = ArrayList()
 
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
@@ -34,5 +52,22 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
 //            textHome.text = it
         })
+
+        noteList.add(NoteModel("Title", "Body"))
+        noteList.add(NoteModel("Title", "Body"))
+        noteList.add(NoteModel("Title", "Body"))
+        noteList.add(NoteModel("Title", "Body"))
+        noteList.add(NoteModel("Title", "Body"))
+
+        Toast.makeText(requireContext(), noteList.size.toString(), Toast.LENGTH_LONG).show()
+        binding.rvNotes.apply {
+            noteAdapter = NoteAdapter(noteList, this@HomeFragment)
+            setHasFixedSize(true)
+            layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+            adapter = noteAdapter
+        }
+    }
+
+    override fun itemClicked(view: View, noteModel: NoteModel) {
     }
 }
