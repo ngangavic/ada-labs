1ab9b6fa7debf30a0a3eba1dd8f053e121f18b57
ngangavic <ngangavictor10@gmail.com>
2022-04-05 10:24:13 +0300

    date and time pickers

--- app/src/main/java/com/ngangavictor/adalabs/ui/add/AddNoteFragment.kt
+++ app/src/main/java/com/ngangavictor/adalabs/ui/add/AddNoteFragment.kt
@@ -1,13 +1,21 @@
 package com.ngangavictor.adalabs.ui.add
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import com.ngangavictor.adalabs.R
+import com.ngangavictor.adalabs.databinding.FragmentAddNoteBinding
 
 class AddNoteFragment : Fragment() {
+
+    lateinit var binding: FragmentAddNoteBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +27,41 @@ class AddNoteFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_note, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_add_note, container, false)
+        return binding.root
     }
 
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.inputDate.setOnFocusChangeListener { view, b ->
+
+            if (view.isFocused ||b) {
+                val datePicker =
+                    MaterialDatePicker.Builder.datePicker()
+                        .setTitleText("Select date")
+                        .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                        .build()
+                datePicker.addOnPositiveButtonClickListener {
+                    binding.inputDate.setText(datePicker.headerText.toString())
+                }
+                datePicker.show(parentFragmentManager, "")
+            }
+        }
+        binding.inputTime.setOnFocusChangeListener { view, b ->
+            if (view.isFocused ||b) {
+            val picker =
+                MaterialTimePicker.Builder()
+                    .setTimeFormat(TimeFormat.CLOCK_24H)
+                    .setHour(12)
+                    .setMinute(10)
+                    .setTitleText("Select time")
+                    .build()
+            picker.addOnPositiveButtonClickListener {
+                binding.inputTime.setText("""${picker.hour}:${picker.minute}""")
+            }
+            picker.show(parentFragmentManager, "")
+        }
+        }
+    }
 }
